15b86e4d649b3f412c9ed1ff179f4f7bb161b896
krzosa <krzosa.karol@gmail.com>
2020-05-02 17:17:26 +0200

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Features
+## General Overview:
 
+1. Split the code into 2 parts, platform layer and the game/application
+2. Compile the game / application into a dll and export functions from it
+3. Copy the dll to a different file(to prevent windows from locking the file) 
+4. Load the dll from that different file at runtime and extract the functions
+5. In a loop keep checking if the dll changed
+   1. If it changed, null all the pointers to the previous dll
+   2. Copy the dll to a different file
+   3. Load the dll from that different file and extract the functions
+
+
+## Notes:
+ Before copying temp dll to main dll make sure to NULL all the pointers that accessed something from the dll
+
+ Compile with /LD flag and also tell the compiler which functions to export with /link /EXPORT:function ... 
+ 
+ There are some additional troubles if you want to have hot reload while debugging with Visual Studio because
+ VStudio is locking the PDB file so the solution is to write to a different file with a different name each time
+ you build the dll
--- src/windows_main.cpp
+++ src/windows_main.cpp
@@ -10,25 +10,6 @@
 global_variable SDL_Window* window = NULL;
 global_variable int global_loop = 1;
 
-/* General Overview:
-   1. Split the code into 2 parts, platform layer and the game/application
-   2. Compile the game / application into a dll and export functions from it
-   3. Copy the dll to a different file(to prevent windows from locking the file) 
-   4. Load the dll from that different file at runtime and extract the functions
-   5. In a loop keep checking if the dll changed
-    6. If it changed, null all the pointers to the previous dll
-    7. Copy the dll to a different file
-    8. Load the dll from that different file and extract the functions
-*/
-
-/* NOTES:
-    Before copying temp dll to main dll make sure to NULL all the pointers that accessed something from the dll
-    Compile with /LD flag and also tell the compiler which functions to export with /link /EXPORT:function ... 
-    There are some additional troubles if you want to have hot reload while debugging with Visual Studio because
-    VStudio is locking the PDB file so the solution is to write to a different file with a different name each time
-    you build the dll
-*/
-
 #define UPDATE_AND_RENDER(name) void name(graphics_buffer* buffer, user_input *input, game_state *gameState)
 typedef UPDATE_AND_RENDER(UpdateAndRender);
 UPDATE_AND_RENDER(UpdateAndRenderStub)
